57b453d2afd5c4967c423edccb43fc9beb39b38c
lomash-choudhary <lomashchoudhary9812@gmail.com>
2024-08-11 19:05:18 +0530

    Delete reading from file.js

--- reading from file.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const contents = fs.readFileSync("a.txt" , "utf-8");
-console.log(contents);
-
-const content = fs.readFileSync("b.txt" , "utf-8");
-console.log(content);
